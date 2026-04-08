e42a918bc1e18b030fe68ab40c5d683f580b1f7e
skytool <skytool@github.com>
2026-04-08 01:14:15 -0500

    fix: TravelSpot/Review Active Storage 충돌 제거

--- app/models/travel_review.rb
+++ app/models/travel_review.rb
@@ -1,8 +1,6 @@
 class TravelReview < ApplicationRecord
   belongs_to :travel_spot
   belongs_to :user
-  has_one_attached :cover_image
-  has_many_attached :photos
   validates :title, presence: true
   validates :content, presence: true
   validates :rating, inclusion: { in: 1..5 }
--- app/models/travel_spot.rb
+++ app/models/travel_spot.rb
@@ -1,7 +1,5 @@
 class TravelSpot < ApplicationRecord
   has_many :travel_reviews, dependent: :destroy
-  has_one_attached :cover_image
-  has_many_attached :photos
   validates :name, presence: true
   validates :country, presence: true
 end
